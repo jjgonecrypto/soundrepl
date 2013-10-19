b11ca33c92b5e2b32adddcd733fb7ff01e6ee0b3
justin j. moses <justinjmoses@gmail.com>
2013-10-19 01:32:08 -0400

    refactoring playing into soundrepl, implementing note array and duration

--- src/tracks/getlucky.js
+++ src/tracks/getlucky.js
@@ -20,20 +20,30 @@
     F: ['c#4', 'f#4', 'a5']
   };
 
+  var progressions = {
+    basic: [{notes: ['e2'], duration: 1}, {notes: ['e4'], duration: 0.75}, {notes: ['e2'], duration: 0.25}, {notes: ['e2'], duration: 1}, {notes: ['e3'], duration: 1}]
+  };
+
   var riffs = {
      main: [chords.B, chords.F],
    //   bassE: getlucky.bassLines.E.beat(getlucky.rhythms.bass1)
   };
 
-  //var getlucky = soundrepl.create();
+  var getlucky = soundrepl.create();
   //getlucky.add(baseLines.E, 'baseline:E').map(rhythms.bass1);
-
-  var getlucky = {
-    rhythms: rhythms,
-    bass: bassLines,
-    chords: chords,
-    riffs: riffs
-  };
+  getlucky.add(rhythms.bass1, 'bass line rhythm 1');
+  getlucky.add(bassLines.E, 'bass line in E');
+  getlucky.add([chords.B, chords.F], 'B/F progression');
+  getlucky.add(progressions.basic, 'basic bass progression');
+  getlucky.add(['b3','d2'], 'double');
+  getlucky.add(['a5'], 'single');
+  
+  // var getlucky = {
+  //   rhythms: rhythms,
+  //   bass: bassLines,
+  //   chords: chords,
+  //   riffs: riffs
+  // };
 
   if (typeof exports !== 'undefined') {
     if (typeof module !== 'undefined' && module.exports) exports = module.exports = getlucky;
