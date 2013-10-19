84485bb810a6bb8070ddeed8b488eb904482ad2c
justin j. moses <justinjmoses@gmail.com>
2013-10-19 17:24:40 -0400

    Adding simple node web server

---
+++ server.js
@@ -0,0 +1,13 @@
+var connect = require('connect');
+var port = process.env.PORT || 5000;
+
+connect()
+  // disabled compress for development where data size isn't enough to make it worth
+  // .use( connect.compress() )
+  .use( connect.logger({format:"dev"}) )
+  .use( connect.static( __dirname + '/') )
+  // also experiencing worse performance in dev while using in-memory caching
+  // .use( connect.staticCache() )
+  .listen( port );
+
+console.log("Connect server listening on port %d", port);
